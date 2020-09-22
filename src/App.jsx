349a5b9401c6c5344bbdb8b8e0640e1d2df57efa
Mohamed Ammar <mohamed.ammar86@gmail.com>
2020-09-22 18:29:33 -0400

    initiated the app, pu the header and the color circle

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 
+class Circle extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
 class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <>
+        <h1>Color Picker!</h1>
+        <div className="parent">
+          <div className="circle"></div>
+        </div>
+      </>
+    )
   }
 }
 
