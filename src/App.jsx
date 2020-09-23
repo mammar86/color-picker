37527e9c5de32986526c3d34be69973925da0f4b
Mohamed Ammar <mohamed.ammar86@gmail.com>
2020-09-22 20:39:11 -0400

    added ranges and styled it in the middle

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,26 @@ class App extends Component {
         <div className="parent">
           <div className="circle"></div>
         </div>
+        <section>
+          <div className="hue">
+            <h4>Hue:</h4>
+            <span>0</span>
+            <input type="range" min="0" max="360"></input>
+            <span>360</span>
+          </div>
+          <div className="saturation">
+            <h4>Saturation:</h4>
+            <span>0</span>
+            <input type="range" min="0" max="100"></input>
+            <span>100</span>
+          </div>
+          <div className="lightness">
+            <h4>Lightness:</h4>
+            <span>0</span>
+            <input type="range" min="0" max="100" value="50"></input>
+            <span>100</span>
+          </div>
+        </section>
       </>
     )
   }
