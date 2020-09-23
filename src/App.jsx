70b071ecafd336c33aa410672c1514b23a7f8904
Mohamed Ammar <mohamed.ammar86@gmail.com>
2020-09-23 00:01:58 -0400

    added inline styling and app is working, will add some improvment in the next step

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 
-class Range extends Component {
+// class Range extends Component {
+//   render() {
+//     return (
+//       <div className={this.props.class}>
+//         <h4>{this.props.title}</h4>
+//         <span>0</span>
+//         <input
+//           type="range"
+//           min="0"
+//           max={this.props.max}
+//           value={this.state.hue}
+//           onChange={this.rangeValueHue}
+//         ></input>
+//         <span>{this.props.max}</span>
+//       </div>
+//     )
+//   }
+// }
+
+class App extends Component {
   state = {
     hue: 0,
     saturation: 0,
@@ -30,34 +49,53 @@ class Range extends Component {
 
   render() {
     return (
-      <div className={this.props.class}>
-        <h4>{this.props.title}</h4>
-        <span>0</span>
-        <input
-          type="range"
-          min="0"
-          max={this.props.max}
-          value={this.state.hue}
-          onChange={this.rangeValueHue}
-        ></input>
-        <span>{this.props.max}</span>
-      </div>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
       <>
         <h1>Color Picker!</h1>
         <div className="parent">
-          <div className="circle"></div>
+          <div
+            className="circle"
+            style={{
+              backgroundColor: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%`,
+            }}
+          ></div>
         </div>
         <section>
-          <Range title="Hue" max="360" class="hue" />
-          <Range title="Saturation" max="100" class="saturation" />
-          <Range title="Lightness" max="100" class="lightness" />
+          <div className="hue">
+            <h4>Hue:</h4>
+            <span>0</span>
+            <input
+              type="range"
+              min="0"
+              max="360"
+              value={this.state.hue}
+              onChange={this.rangeValueHue}
+            ></input>
+            <span>360</span>
+          </div>
+          <div className="saturation">
+            <h4>Saturation:</h4>
+            <span>0</span>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={this.state.saturation}
+              onChange={this.rangeValueSaturation}
+            ></input>
+            <span>100</span>
+          </div>
+          <div className="lightness">
+            <h4>Lightness:</h4>
+            <span>0</span>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={this.state.lightness}
+              onChange={this.rangeValueLightness}
+            ></input>
+            <span>100</span>
+          </div>
         </section>
       </>
     )
